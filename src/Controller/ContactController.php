476299bb9f7d53fd72789889065ea795b1b38e48
MorganRedbox <morgan.pousson@redbox.fr>
2024-10-04 14:51:34 +0200

    final adjustements

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            // Send email
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('your-email@example.com') // Change this to your email
+                ->subject('Contact Form Submission')
+                ->text($data['message']);
+
+            $mailer->send($email);
+
+            // Optionally add a flash message and redirect
+            $this->addFlash('success', 'Your message has been sent!');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
